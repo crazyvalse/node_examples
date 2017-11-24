fe24833c410d892bf4f5abeaa74235a0f056909f
IsaacGu <st.isaac.gu@me.com>
2017-11-24 14:24:33 +0800

    测试提交第一个例子

---
+++ 01/01_HelloWorld.js
@@ -0,0 +1,16 @@
+/**
+ * Created by codingnuts on 2017/11/24.
+ */
+
+var http = require("http");
+
+var server = http.createServer(function (req, res) {
+	//req 请求， res 相应
+	res.writeHead(200, {"Content-type": "text/html;charset=UTF-8"});
+	res.end("yo, 这是Hello World");
+});
+
+server.listen(3000, "127.0.0.1");
+
+//node 01_HelloWorld
+//http://127.0.0.1:3000
