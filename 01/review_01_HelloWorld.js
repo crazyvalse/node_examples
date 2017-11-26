e20a3a384a8a5af3bb603c55cabab9e470d4face
IsaacGu <st.isaac.gu@me.com>
2017-11-26 11:37:36 +0800

    review

---
+++ 01/review_01_HelloWorld.js
@@ -0,0 +1,17 @@
+/**
+ * Created by codingnuts on 2017/11/26.
+ */
+
+//create http instance
+const http = require('http');
+
+//create server instance
+http.createServer(function (request, response) {
+	response.writeHead(200, {
+		"Content-Type": "text/html",
+		"charset": "UTF8"
+	});
+	response.write("<h1>Hello World </h1>");
+	response.end();
+
+}).listen(3000, 'localhost');
